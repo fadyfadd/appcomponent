9977401f428f3bef6efc8c6ce26a8ff7263ef9f0
fadyfadd <fady_faddoul@hotmail.com>
2022-01-15 13:59:55 -0500

    initial commit

---
+++ apps.fady.org/src/main/java/test/fady/org/MyApp.java
@@ -0,0 +1,37 @@
+package test.fady.org;
+
+import static org.junit.Assume.assumeNoException;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Deque;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+
+ 
+public class MyApp {
+ 
+	public static void main(String[] args) { 
+			
+
+	}
+		
+}
+
+
