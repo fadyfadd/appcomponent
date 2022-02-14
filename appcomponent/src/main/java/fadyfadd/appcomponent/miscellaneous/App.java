e7e410ada009345c9a78f4726dfee7684a6212a4
fadyfadd <fady_faddoul@hotmail.com>
2022-02-14 09:46:19 -0500

    cleanup

---
+++ appcomponent/src/main/java/fadyfadd/appcomponent/miscellaneous/App.java
@@ -0,0 +1,13 @@
+package fadyfadd.appcomponent.miscellaneous;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+	}
+
+}
